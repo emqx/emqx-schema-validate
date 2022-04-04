51c2c9fff0e8a923a5b5cd86100682786de518ad
ieQu1 <99872536+ieQu1@users.noreply.github.com>
2022-04-05 00:07:50 +0200

    Change missing description to an error

--- src/emqx_schema_validate.erl
+++ src/emqx_schema_validate.erl
@@ -26,21 +26,12 @@ main(_) ->
 process_data(Binary) ->
     {ok, _} = application:ensure_all_started(emqx_schema_validate),
     langtool:start(),
-    ets:new(?TAB, [named_table, {keypos,1}]),
     Data = jsone:decode(Binary, [{object_format, map}, {keys, atom}]),
     spellcheck_schema(Data),
-    format_undocumented_stats(),
     case is_ok() of
         true -> halt(0);
         false -> halt(1)
     end.
-
-format_undocumented_stats() ->
-    Stats = lists:keysort(2, ets:tab2list(?TAB)),
-    {_, Vals} = lists:unzip(Stats),
-    Total = lists:sum(Vals),
-    io:format(user, "~nUndocumented values ~p:~n", [Total]),
-    [io:format(user, "~-40.. s ~p~n", [Root, Val]) || {Root, Val} <- lists:reverse(Stats)].
 
 spellcheck_schema(Data) ->
     [begin
@@ -68,12 +59,12 @@ do_spellcheck(FullName, #{desc := Desc}) ->
 %% Ignore references to structs, since the struct itself should have a description
 do_spellcheck(FullName, #{type := #{kind := <<"struct">>}}) ->
     ok;
-do_spellcheck(<<"Root Config Keys">>, _) ->
+do_spellcheck([<<"Root Config Keys">>], _) ->
     ok;
 do_spellcheck(FullName, _) ->
     Record = hd(FullName),
-    ets:update_counter(?TAB, Record, {2, 1}, {Record, 0}),
-    io:format(user, "Warning: '~s' doesn't have a description~n", [format_name(FullName)]),
+    io:format(user, "Error: '~s' doesn't have a description~n", [format_name(FullName)]),
+    setfail(),
     ok.
 
 read_stdio() ->
